4903d3c08e65eb3793a117d4a692c943da9d1ae2
Alee_717 <alehernandez028h@gmail.com>
2025-02-13 20:24:59 -0600

    HomeScreenView

--- components/features/home/homeView.tsx
+++ components/features/home/homeView.tsx
@@ -1,9 +1,124 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 export function HomeView(){
-      return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-          <Text>Pantalla de Inicio</Text>
+
+    const level = 'red'
+
+    const getLevelColor = () => {
+        switch (level) {
+            case "red":
+                return styles.high;
+            {/*break;
+            case "red":
+                return styles.medium;
+            break;
+            case "red":
+                return styles.high;
+            break;
+            default:
+                return 0;
+            break;*/}
+        }
+    }
+
+    return(
+        <View style={styles.body}>
+            <View style={styles.box_Header}>
+                <View style={styles.box1_Header}>
+                    <Image
+                    style={styles.image}
+                        source={require("../../../assets/images/sol.png")}
+                    />
+                </View>
+                <View style={styles.box2_Header}>
+                    <Text style={styles.text1_Header}>Probabilidad de desbordamientos</Text>
+                    <Text style={[getLevelColor()]}>Alta</Text>
+                </View>
+            </View>
+
+            <View style={styles.container_Info}>
+                <View style={styles.box1_Container}></View>
+                <View style={styles.box1_Container}></View>
+            </View>
+            <View style={styles.container_graphic}>
+
+            </View>
         </View>
-      );
+    );
 }
+
+const styles = StyleSheet.create({
+    body:{
+        backgroundColor: '#9fc5f8',
+        height: '100%',
+        width: '100%',
+    },
+    box_Header:{
+        flexDirection: 'row',
+        marginTop: 20,
+        padding: 15,
+    },
+    box1_Header:{
+        width: '50%',
+        height: 'auto',
+        justifyContent: 'center',
+        alignContent: 'center',
+    },
+    image:{
+        height: 170,
+        width: 170,
+    },
+    box2_Header:{
+        width: '50%',
+        height: 'auto',
+        justifyContent: 'center',
+        alignContent: 'center',
+    },
+    text1_Header:{
+        textAlign: 'center',
+        fontSize: 18,
+    },
+    low:{
+        color: 'green',
+        textAlign: 'center',
+        fontSize: 25,
+        marginTop: 15
+    },
+    high:{
+        color: 'red',
+        textAlign: 'center',
+        fontSize: 25,
+        marginTop: 15,
+    },
+    medium:{
+        color: 'yellow',
+        textAlign: 'center',
+        fontSize: 25,
+        marginTop: 15,
+    },
+    container_Info:{
+        flexDirection: 'row',
+        padding: 15,
+        gap: 15,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    box1_Container:{
+        height: 250,
+        width: 180,
+        borderWidth: 2,
+        borderColor: '#000000',
+        borderRadius: 20,
+        backgroundColor: 'rgba(255, 255, 255, 0.5)'
+    },
+    container_graphic:{
+        height: 200,
+        width: 380,
+        borderWidth: 2,
+        borderColor: '#000000',
+        borderRadius: 20,
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        marginLeft: 15,
+    },
+})
