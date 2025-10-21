697bc84489ade918bfa7fa2dbb79fe09cff0f39a
Angel Gutierrez <gadea2005@hotmail.com>
2025-10-20 22:35:17 -0600

    Fix case-sensitive folder names

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://pvbxycfrdbsxhkvdjzyl.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2Ynh5Y2ZyZGJzeGhrdmRqenlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk4MjI3NjksImV4cCI6MjA1NTM5ODc2OX0.L-o_wReQ-BWjH17xwiM__18dq1Un13Wf3QJPOToMQh4';
+const supabaseUrl = 'https://eokkdouvyvovyhdljeix.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVva2tkb3V2eXZvdnloZGxqZWl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5NzkzMTUsImV4cCI6MjA3NjU1NTMxNX0.Nr6zt2_zAkr-JRrxurNHvVOHVmzDV4UInKUn-EjgB2s';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
